2a3c27c10e0fc51448012fc12c257928fd53b4ee
mmraihan <mm_raihan@live.com>
2021-11-19 00:54:04 +0600

    Necessary Packages,ConnectionString, Add Book Model

--- BookListRazor-Updated/BookListRazor/Program.cs
+++ BookListRazor-Updated/BookListRazor/Program.cs
@@ -12,7 +12,7 @@ namespace BookListRazor
     public class Program
     {
         public static void Main(string[] args)
-        {
+        {   
             CreateHostBuilder(args).Build().Run();
         }
 
